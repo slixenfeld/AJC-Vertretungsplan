55488759f52336f5d3e9f7514736ad436f85a26e
slxfld <31144712+slxfld@users.noreply.github.com>
2021-11-17 20:14:18 +0100

    v1.5 App Upload

--- app/src/main/java/com/example/ajc_vertretung/InputOutput.java
+++ app/src/main/java/com/example/ajc_vertretung/InputOutput.java
@@ -7,21 +7,15 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class InputOutput {
 
     public static final String FILE_NAME = "ajcv-config.txt";
-    public String configKlasse = "";
+    public ArrayList<String> configLines = new ArrayList<String>();
 
     public InputOutput() {
 
     }
 
-    public String getConfigKlasse() {
-        return configKlasse;
-    }
-
-    public void setConfigKlasse(String configKlasse) {
-        this.configKlasse = configKlasse;
-    }
 }
